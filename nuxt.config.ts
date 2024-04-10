08feb6709896891686726b1f2edc838d1792c740
Alan Duchêne <alan.duchene@zenika.com>
2024-04-10 09:07:41 +0200

    feat: env vars

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // You can configure fully typed, per-environment overrides in your nuxt.config
+  $production: {
+  },
+  $development: {
+  },
+  // can be overloaded in .env files
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: '123',
+    secretOverloadedInDotEnvFile: 'default value',
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: '/api',
+      publicOverloadedInDotEnvFile: ''
+    }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>Affichage des variables d'environnements accessible depuis le client:</div>
+    <div>Public: {{ publicVars }}</div>
+    <div>Secret: {{ secretVars }}</div>
+</template>
+
+<script setup lang="ts">
+    const { public: publicVars, app, nitro, ...secretVars} = useRuntimeConfig()
+</script>
