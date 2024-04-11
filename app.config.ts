a3811cc50169f1b01f1db46557eece860ba2536b
Alan Duchêne <alan.duchene@zenika.com>
2024-04-11 05:54:35 +0200

    feat: app config public vars

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+export default defineAppConfig({
+    title: 'Nuxt render strategies',
+    theme: {
+        dark: true,
+        colors: {
+            primary: '#ff0000'
+        }
+    },
+    copyright: 'aland404'
+})
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-    <div>Affichage des variables d'environnements accessible depuis le client:</div>
+    <div>_Environment variables_</div>
+    <div>Secrets, only available server-side</div>
+    <div>{{ secretVars }}</div>
+    <div>Publics, are also available client-side</div>
     <div>Public: {{ publicVars }}</div>
-    <div>Secret: {{ secretVars }}</div>
+    <br/>
+    <div>_Public variables that can be determined at build time_</div>
+    <div>{{ appConfig }}</div>
 </template>
 
 <script setup lang="ts">
     const { public: publicVars, app, nitro, ...secretVars} = useRuntimeConfig()
+    const {nuxt, ...appConfig} = useAppConfig()
 </script>
