70d74cbb7cf618018a764511677e8be115385cfa
Alan Duchêne <alan.duchene@zenika.com>
2024-04-14 09:12:01 +0200

    --no-verify

--- pages/custom/index.vue
+++ pages/custom/index.vue
@@ -1,17 +1,18 @@
 <template>
     <div><strong>This is my /custom page content with a custom layout</strong></div>
-    <button @click="toggleLayout">Toggle layout</button>
+    <button @click="toggleLayout">Toggle {{ layoutToToggleName }} layout</button>
 </template>
 
 
 <script setup>
-    definePageMeta({
-        layout:'custom-layout'
-    })
+definePageMeta({
+    layout:'custom-layout'
+})
 
-    const route = useRoute()
+const route = useRoute()
+const layoutToToggleName = route.meta.layout === 'default' ? 'custom-layout' : 'default'
 
-    const toggleLayout = () => {
-        setPageLayout(route.meta.layout === 'default' ? 'custom-layout' : 'default')
-    }
+const toggleLayout = () => {
+    setPageLayout(layoutToToggleName)
+}
 </script>
